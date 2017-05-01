d7140a60d4ddad43692b67821dee067d5d26d3fc
gdmcdonald <gordon27@gmail.com>
2017-05-01 13:46:29 +1000

    create AddColHere.r
    Function to add a column in the middle of a dataframe 
    e.g.
    df<-[a=1:3,b=4:6,c=7:9]
    myCol<-0:2
    AddColHere(myCol,df,afterCol = "b")
    output:
      a b myCol c
    1 1 4         0 7
    2 2 5        1 8
    3 3 6        2 9

---
+++ AddColHere.r
@@ -0,0 +1,38 @@
+AddColHere<-function(addCol,DF,afterCol=NULL,beforeCol=NULL ){
+  if(!is.null(afterCol)){
+    if(is.null(beforeCol)) {
+      #afterCol populated, beforeCol not
+      afterColNumber<-which(names(DF)==afterCol)
+      ldf<-length(DF)
+      
+      DF<-cbind(DF,addCol) #add col to end of df then move
+      if(afterColNumber<ldf){
+        DF<-DF[,c(1:afterColNumber,ldf+1,(afterColNumber+1):ldf)]
+      }
+    } else {
+      #aftercol populated, beforecol populated
+      stop("should only have one of beforeCol & afterCol")
+    }
+  }else{
+    if(is.null(beforeCol)) {
+      #if both before and after are null, just add it on the end
+      DF<-cbind(DF,addCol)
+    } else {
+      
+      
+      # beforeCol populated, afterCol not
+      beforeColNumber<-which(names(DF)==beforeCol)
+      ldf<-length(DF)
+      DF<-cbind(addCol,DF) #add col to beginning of DF then move, 
+      #saves some conditional logic compared to adding it after.
+      if(beforeColNumber>1){
+        DF<-DF[,c(2:beforeColNumber,1,(beforeColNumber+1):(ldf+1))]
+      }
+    }
+    
+    
+  }
+  names(DF)[names(DF) == 'addCol'] <- as.character(substitute(addCol))
+  return(DF)
+  
+}
