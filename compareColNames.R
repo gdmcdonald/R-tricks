8abcfdf9b9fbd3adbcc8b67ea7cf9c43746c806c
Gordon McDonald <gordon27@gmail.com>
2017-06-07 12:45:04 +1000

    Update compareColNames.R

--- compareColNames.R
+++ compareColNames.R
@@ -21,5 +21,5 @@ rbindSimmilar<-function(df1,df2){
 
 # Example:
 # df1=data.frame(a=1:3,b=2:4,c=4:6)
-# df2=data.frame(a=1:5,b=6:10,d=10:14)
+# df2=data.frame(a=1:2,b=6:7,d=10:11)
 # rbindSimmilar(df1,df2)
