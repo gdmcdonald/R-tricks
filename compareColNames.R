399d2c1e457c134201c948d01b224d5f1c48129a
Gordon McDonald <gordon27@gmail.com>
2017-06-07 12:38:10 +1000

    add rbind simmilar

--- compareColNames.R
+++ compareColNames.R
@@ -4,3 +4,22 @@ compareColNames<-function(df1,df2){
   print("These columns are in the second but not the first df:")
   print(names(df2)[!(names(df2) %in% names(df1))])
 }
+
+# Example:
+# df1=data.frame(a=1:3,b=2:4,c=4:6)
+# df2=data.frame(a=1:5,b=6:10,d=10:14)
+# compareColNames(df1,df2)
+
+
+rbindSimmilar<-function(df1,df2){
+  output<-rbind(df1[,(names(df1) %in% names(df2))],
+        df2[,(names(df2) %in% names(df1))])
+  print("These different columns were not carried through to the output:")
+  compareColNames(df1,df2)
+  return(output)
+}
+
+# Example:
+# df1=data.frame(a=1:3,b=2:4,c=4:6)
+# df2=data.frame(a=1:5,b=6:10,d=10:14)
+# rbindSimmilar(df1,df2)
